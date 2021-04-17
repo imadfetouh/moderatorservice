d979579cec583e67b7fd661f1325c01e16f20d7f
imad <imad.elfetouh@hotmail.com>
2021-04-17 11:28:56 +0200

    test

--- moderatorservice/src/test/java/com/imadelfetouh/adminservice/ModeratorserviceApplicationTests.java
+++ moderatorservice/src/test/java/com/imadelfetouh/adminservice/ModeratorserviceApplicationTests.java
@@ -2,8 +2,8 @@ package com.imadelfetouh.adminservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
 class ModeratorserviceApplicationTests {
 
     @Test
